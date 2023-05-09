adad93630a78c761e3f8bddb8e81da3464906b22
ap211unitech <porwal.1@iitj.ac.in>
2023-05-09 13:07:09 +0530

    PWA app made

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
+
+const nextConfig = withPWA({
   reactStrictMode: false,
   devIndicators: {
     buildActivity: false
   }
-}
+})
 
 module.exports = nextConfig
